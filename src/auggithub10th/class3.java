99fe84bbbb02f19347f1e43e41f09f773eed990b
ShiurkarS <shiurkarshubham@gmail.com>
2024-12-10 18:16:38 +0530

    CommitMsg

---
+++ src/auggithub10th/class3.java
@@ -0,0 +1,9 @@
+package auggithub10th;
+
+public class class3 {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("HOI FROM SHUBHAM SHIURKAR");
+		Thread.sleep(3000);
+	}
+
+}
